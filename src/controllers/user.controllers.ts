33ab90d962fcbe90e10e653055b0f6bd5a85a245
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-12 13:29:37 +0530

    added registerUser

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -5,7 +5,46 @@ import { CustomError } from "../utils/CustomError";
 import { ResponseStatus } from "../utils/constants";
 import { User } from "../models/user.models";
 import { ApiResponse } from "../utils/ApiResponse";
+import { sendVerificationMail } from "../utils/sendMail";
+import { handleZodError } from "../utils/handleZodError";
 
-const registerUser = asyncHandler(async (req: Request, res: Response) => {});
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password, username, avatar, fullName } = handleZodError(
+    validateRegisterData(req.body)
+  );
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new CustomError(ResponseStatus.Conflict, "Email already registered");
+  }
+
+  let user = await User.create({ email, password, username, avatar, fullName });
+
+  if (!user) {
+    throw new CustomError(
+      ResponseStatus.InternalServerError,
+      "User registration failed"
+    );
+  }
+
+  const { hashedToken, tokenExpiry, unHashedToken } = user.generateToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+  await user.save();
+
+  await sendVerificationMail(user.username, user.email, unHashedToken);
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        ResponseStatus.Success,
+        {},
+        "User registered successfully. Please verify your email"
+      )
+    );
+});
 
 export { registerUser };
---
+++ src/utils/handleZodError.ts
@@ -0,0 +1,27 @@
+import { SafeParseReturnType } from "zod";
+import { CustomError } from "./CustomError";
+import { ResponseStatus } from "./constants";
+
+const handleZodError = (result: SafeParseReturnType<any, any>) => {
+  if (!result.success) {
+    const missing = result.error.issues.find(
+      (issue) => issue.code === "invalid_type" && issue.received === "undefined"
+    );
+
+    if (missing) {
+      throw new CustomError(
+        ResponseStatus.BadRequest,
+        "Missing required fields"
+      );
+    }
+
+    throw new CustomError(
+      ResponseStatus.BadRequest,
+      result.error.issues[0].message
+    );
+  }
+
+  return result.data;
+};
+
+export { handleZodError };
