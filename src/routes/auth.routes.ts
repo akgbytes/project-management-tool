9fd51656a622423a9a208dc788edef21c083d0f9
Aman Kumar Gupta <akgbytes@gmail.com>
2025-05-03 01:38:04 +0530

    feat: implement rate limiting for authentication and email requests

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -14,13 +14,17 @@ import {
 } from "../controllers/auth.controllers";
 import { isLoggedIn } from "../middlewares/auth.middlewares";
 import { upload } from "../middlewares/multer.middlewares";
+import {
+  authLimiter,
+  emailsLimiter,
+} from "../middlewares/rateLimiter.middlewares";
 
 router.post("/register", upload.single("avatar"), registerUser);
 router.get("/verify/:token", verifyUser);
-router.post("/resend-email", resendVerificationEmail);
-router.post("/login", loginUser);
+router.post("/resend-email", emailsLimiter, resendVerificationEmail);
+router.post("/login", authLimiter, loginUser);
 router.post("/logout", isLoggedIn, logoutUser);
-router.post("/password/forgot", forgotPassword);
+router.post("/password/forgot", emailsLimiter, forgotPassword);
 router.post("/password/reset/:token", resetPassword);
 router.get("/refresh-tokens", refreshAccessToken);
 router.get("/me", isLoggedIn, getMe);
